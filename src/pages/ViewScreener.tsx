cfccaf52df0c5d2f6900359656a68d0474e4d6de
ketul-andev <ketul@adnabu.com>
2025-05-15 23:57:30 +0530

    Added API call to Queue

--- src/pages/ViewScreener.tsx
+++ src/pages/ViewScreener.tsx
@@ -1,16 +1,50 @@
+import { useApp } from "@/contexts/AppContext";
 import axiosInstance from "@/lib/axios";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { NavigationBar } from "@/components/NavigationBar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+
+const stockData = [
+    {
+        stock: "AAPL",
+        price: 150.75,
+        change: 1.25,
+        volume: 1000000
+    },
+    {
+        stock: "AAPL",
+        price: 150.75,
+        change: 1.25,
+        volume: 1000000
+    },
+    {
+        stock: "AAPL",
+        price: 150.75,
+        change: 1.25,
+        volume: 1000000
+    },
+    {
+        stock: "AAPL",
+        price: 150.75,
+        change: 1.25,
+        volume: 1000000
+    }
+]
+
 
 export default function ViewScreener() {
     const { id } = useParams();
     const [screener, setScreener] = useState<any>(null);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
-    
+    const { userData } = useApp();
+    const [jobId, setJobId] = useState<string | null>(null);
+    const context = useApp();
+
     useEffect(() => {
             axiosInstance.get(`/screeners`, {
                 params: {
@@ -27,6 +61,20 @@ export default function ViewScreener() {
                 setLoading(false);
             });
     }, []);
+
+    const handleRunScreener = () => {
+        if (userData) {
+            axiosInstance.post('/jobs', {
+                rules: screener.rules,
+                username: userData.username
+            }).then((res) => {
+                setJobId(res.data.job_id);
+                context.showToast("Screener running", "success");
+            }).catch((err) => {
+                context.showToast("Error running screener", "error");
+            });
+        }
+    }
 
     return (
         <div>
@@ -61,8 +109,66 @@ export default function ViewScreener() {
                             </CardContent>
                         </Card>
                     </div>
-                    <div className="flex flex-col items-center mx-10 my-10">
-                        <h1>Rules</h1>
+                    <div className="flex flex-row outline outline-gray-300 rounded-lg mx-10 my-10">
+                        <div className="flex flex-col items-center mx-10 my-10 w-1/5">
+                            <Button variant="outline" onClick={handleRunScreener}>
+                                <h1>Run Screener</h1>
+                            </Button>
+                        </div>
+                        <div className="flex flex-col items-center mx-10 my-10 w-4/5">
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead className="w-24 text-center">Stock</TableHead>
+                                        <TableHead className="w-24 text-center">Price</TableHead>
+                                        <TableHead className="w-24 text-center">Change</TableHead>
+                                        <TableHead className="w-24 text-center">Volume</TableHead>
+                                        <TableHead className="w-24 text-center"></TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {stockData.map((stock) => (
+                                        <TableRow>
+                                            <TableCell className="text-center">{stock.stock}</TableCell>
+                                            <TableCell className="text-center">{stock.price}</TableCell>
+                                            <TableCell className="text-center">{stock.change}</TableCell>
+                                            <TableCell className="text-center">{stock.volume}</TableCell>
+                                            <TableCell className="text-center">
+                                                <Button variant="link" className="text-blue-500">
+                                                    Add to Watchlist
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                    {stockData.map((stock) => (
+                                        <TableRow>
+                                            <TableCell>{stock.stock}</TableCell>
+                                            <TableCell>{stock.price}</TableCell>
+                                            <TableCell>{stock.change}</TableCell>
+                                            <TableCell>{stock.volume}</TableCell>
+                                            <TableCell>
+                                                <Button variant="link" className="text-blue-500">
+                                                    Add to Watchlist
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                    {stockData.map((stock) => (
+                                        <TableRow>
+                                            <TableCell>{stock.stock}</TableCell>
+                                            <TableCell>{stock.price}</TableCell>
+                                            <TableCell>{stock.change}</TableCell>
+                                            <TableCell>{stock.volume}</TableCell>
+                                            <TableCell>
+                                                <Button variant="link" className="text-blue-500">
+                                                    Add to Watchlist
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </div>
                     </div>
                 </div>
             )}
