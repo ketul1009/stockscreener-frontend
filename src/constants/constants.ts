27c4b8acc7da5d0b6e8213253772929fe7cc6fce
ketul-andev <ketul@adnabu.com>
2025-05-19 23:00:04 +0530

    changed to use URL from env

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 import indicators from "./indicators.json";
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export const API_KEY = "your_api_key_here";
 
